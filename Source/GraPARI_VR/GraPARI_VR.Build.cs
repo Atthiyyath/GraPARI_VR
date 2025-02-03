1c3bbc157f8245abe2fc50abd89409227653b447
Atia <atthiyya@gmail.com>
2025-02-03 21:48:24 +0700

    Init VRE

--- Source/GraPARI_VR/GraPARI_VR.Build.cs
+++ Source/GraPARI_VR/GraPARI_VR.Build.cs
@@ -8,7 +8,7 @@ public class GraPARI_VR : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "VRExpansionPlugin" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
